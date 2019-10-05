b1a5256b1c9b92b982d9c80740f30703a95ce57f
s3722763 <s3722763@student.rmit.edu.au>
2019-10-06 01:48:06 +1000

    I think the code now works

--- AssignmentAssembly/main.asm
+++ AssignmentAssembly/main.asm
@@ -4,74 +4,43 @@
 ; Created: 20/09/2019 11:32:22
 ; Author : Daniel
 ;
-.def IS_HIGH=r16
-.def GOT_DISTANCE=r17
-.def temp=r18
-.def count_low=r19
-.def count_high=r20
+.def zero_reg=r20
+.def sreg_temp=r11
+.def IS_HIGH=r18
+.def GOT_DISTANCE=r19
+.def temp=r21
+.def pulse_time_low=r16
+.def pulse_time_high=r17
+.def distance=r12
+.def div_result=r25
 
 ; Replace with your application code
-reset:
+.ORG 0x00
    rjmp start ; Reset interrupt
-   reti      ; Addr $01
-   reti      ; Addr $02
-   reti      ; Addr $03
-   reti      ; Addr $04
-   reti      ; Addr $05
-   reti      ; Addr $06        Use 'rjmp myVector'
-   reti      ; Addr $07        to define a interrupt vector
-   reti      ; Addr $08
-   reti      ; Addr $09
-   reti      ; Addr $0A
-   reti      ; Addr $0B        This is just an example
-   reti      ; Addr $0C        Not all MCUs have the same
-   reti      ; Addr $0D        number of interrupt vectors
-   reti      ; Addr $0E
-   reti      ; Addr $0F
-   reti      ; Addr $10
-
-;Int1 routine
-int1_int_start:
-	CPI IS_HIGH, 0x01
-	BREQ int1_stop_timer
-	RJMP int1_start_timer
-
-int1_stop_timer:
-	;Stop Timer
-	LDI temp, 0x00
-	OUT TCCR1B, temp
-	;Mark the the next change to expect is a high pulse
-	LDI IS_HIGH, 0x00
-	;Mark that the distance can now be calculated
-	LDI GOT_DISTANCE, 0x01
-	RJMP int1_int_end
-
-int1_start_timer:
-	;Start timer with an 8 prescale factor (8 clocks = 1 count)	
-	LDI temp, (1 << CS11)
-	;Mark that the next change should be a high to low change
-	LDI IS_HIGH, 0x01
-	RJMP int1_int_end
-
-int1_int_end:
-	RETI
+.ORG 0x04
+   RJMP int1_int_start       ; Addr $04
 
 start:
-	;Initalize GPIO registers for use
-    LDI temp, 0x01
-	OUT DDRB, temp
-	OUT DDRC, temp
-	LDI temp, 0xF7
-	OUT DDRD, temp
-	LDI temp, 0x00
-	OUT PORTB, temp
-	OUT PORTC, temp
-	
 	;Initialize stack pointer
 	LDI temp, high(RAMEND)
 	OUT SPH, temp
 	LDI temp, low(RAMEND)
 	OUT SPL, temp
+	
+	;Initalize GPIO registers for use
+	;TEMP to test ultrasonic 
+	LDI temp, 0x01
+	OUT DDRC, temp
+	LDI temp, 0xFF
+	OUT DDRB, temp
+	CLR zero_reg
+	;
+
+	LDI temp, 0xF7
+	OUT DDRD, temp
+	LDI temp, 0x00
+	OUT PORTB, temp
+	OUT PORTC, temp
 	RJMP setup_lcd
 
 setup_lcd:
@@ -79,35 +48,124 @@ setup_lcd:
 
 setup_interrupts:
 	;Enable INT1 interrupt
-	LDI temp, (1 << INT1)
+	LDI temp, 0x80
 	OUT GICR, temp
 	;Set INT1 to trigger on any logical change
-	LDI temp, (1 << ISC10)
+	LDI temp, 0x04
 	OUT MCUCR, temp
 	;Enable global interrupts
 	SEI
 	RJMP main_loop
 
 main_loop:
-	RJMP query_sensors
+	RCALL query_sensors
+	RJMP delay
+
+delay:
+    ldi  r18, 31
+    ldi  r19, 113
+    ldi  r20, 31
+L1: dec  r20
+    brne L1
+    dec  r19
+    brne L1
+    dec  r18
+    brne L1
+    nop
+	rjmp main_loop
 
 query_sensors:
 	;Start high pulse
 	LDI temp, 0x01
-	OUT PORTB, temp
+	OUT PORTC, temp
 	;10 us delay
-	ldi  temp, 40
-L1: dec  temp
-    brne L1
+	LDI  temp, 0x28
+L2: DEC  temp
+    BRNE L2
 	;Continue by setting output to low
+	LDI temp, 0x00
+	OUT PORTC, temp
 	LDI GOT_DISTANCE, 0x00
 	;Save a clock cycle by reusing the fact that got_distance is 0x00
-	OUT PORTB, GOT_DISTANCE 
+	;OUT PORTC, GOT_DISTANCE 
 ;Wait until ultrasonic sensor has finished its echo pulse
 wait_until_done:
-	CPI GOT_DISTANCE, 0
+	CPI GOT_DISTANCE, 0x00
 	BREQ wait_until_done
+	;LDI pulse_time_low, 0x80
+	;LDI pulse_time_high, 0x03
 
-	
-	
-	
+	;Make sure old records are gone
+	;CLR pulse_time_low
+	;CLR pulse_time_high
+	;Record the distance into register
+	IN temp, SREG
+	CLI
+	IN pulse_time_low, TCNT1L
+	IN pulse_time_high, TCNT1H	
+
+	OUT TCNT1H, zero_reg
+	OUT TCNT1L, zero_reg
+	OUT SREG, temp
+	;Reset the clock
+	;OUT PORTB, pulse_time_low
+
+	CLR div_result
+	CLR distance
+
+	LDI temp, 0x01
+
+
+check_div_done:
+	;Check if less than 57
+	CPI pulse_time_low, 0x57
+	BRLO check_high
+	;Not done yet
+	RJMP calculate_distance
+
+check_high:
+	CPI pulse_time_high, 0x00
+	;go to display
+	BREQ display_distance
+
+calculate_distance:
+	SUBI pulse_time_low, 0x57
+	SBCI pulse_time_high, 0x00
+	CLC
+	ADD distance, temp
+	RJMP check_div_done
+
+display_distance:
+	OUT PORTB, distance
+	RET
+
+;Int1 routine
+int1_int_start:
+	IN sreg_temp,SREG
+	PUSH temp
+	CPI IS_HIGH,0x01
+	BRNE INT1_NOT_HIGH
+
+INT1_HIGH:
+	;Turn off counter/timer
+	OUT TCCR1B,zero_reg
+
+	;Got the distance now
+	LDI GOT_DISTANCE, 0x01
+	;Pulse now low
+	LDI IS_HIGH,0x00
+	RJMP END_INT1
+
+INT1_NOT_HIGH:
+	IN temp, TCCR1B
+	;Load timer with prescale factor of 8
+	ORI temp,0x02 
+	;Start timer
+	OUT TCCR1B,temp
+	;Echo pulse now high
+	LDI IS_HIGH,0x01
+
+END_INT1:
+	POP temp
+	OUT SREG,sreg_temp
+	RETI
